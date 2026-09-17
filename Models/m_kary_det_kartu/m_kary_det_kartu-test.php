286eb3c632d1486e10082e933339e00703e1d41e
Rey Cannavaro <reyjunoalcannavaro@gmail.com>
2026-09-17 14:19:20 +0700

    feat: add migration, tests, and models for m_kary_det_kartu

---
+++ Models/m_kary_det_kartu/m_kary_det_kartu-test.php
@@ -0,0 +1,71 @@
+<?php
+namespace Tests;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Laravel\Passport\Passport;
+use App\Models\Defaults\User;
+
+class mKaryDetKartuTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testReadingData()
+    {
+        Passport::actingAs(User::first());
+        $payload = [
+            'paginate' => 25
+        ];
+
+        $this->call('GET', '/operation/m_kary_det_kartu', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        ff( $responseArr, 'dump data' );
+
+        $this->assertTrue(true);
+    }
+
+    public function testCreatingData()
+    {
+        $user = User::where('username', 'USERNAME')->first();
+        $this->assertNotEmpty( $user );
+        
+        Passport::actingAs($user);
+
+        $payload = [
+		    "id" => "bigint:optional:autocreate",
+		    "m_kary_id" => "bigint:optional",
+		    "m_comp_id" => "bigint:optional",
+		    "m_dir_id" => "bigint:optional",
+		    "ktp_no" => "string:25:optional",
+		    "ktp_foto" => "string:191:optional",
+		    "pas_foto" => "string:191:optional",
+		    "kk_no" => "string:25:optional",
+		    "kk_foto" => "string:191:optional",
+		    "npwp_no" => "string:25:optional",
+		    "npwp_foto" => "string:191:optional",
+		    "npwp_tgl_berlaku" => "date:optional",
+		    "bpjs_tipe_id" => "bigint:optional",
+		    "bpjs_no" => "string:30:optional",
+		    "bpjs_foto" => "string:191:optional",
+		    "berkas_lain" => "string:191:optional",
+		    "desc_file" => "text:optional",
+		    "creator_id" => "bigint:optional",
+		    "last_editor_id" => "bigint:optional",
+		    "created_at" => "datetime:optional:autocreate",
+		    "updated_at" => "datetime:optional:autocreate",
+		    "bpjs_no_kesehatan" => "string:191:optional",
+		    "bpjs_no_ketenagakerjaan" => "string:191:optional"
+		];
+
+        $this->call('POST', '/operation/m_kary_det_kartu', $payload);
+
+        $responseArr = json_decode( $this->response->getContent(),true );
+        // ff( $responseArr, 'dump data' );
+
+        $this->assertEquals( 200, $this->response->status() );
+        // $this->seeJsonStructure( ['status'] );
+
+        $this->seeInDatabase('m_kary_det_kartu', array_filter($payload, function($dt){
+            return !is_array($dt);
+        } ));
+    }
+}
